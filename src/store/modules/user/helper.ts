32dfe8d805a59af7c0a1eeec6affc4913eab765c
zhiyuan250 <110282795+zhiyuan250@users.noreply.github.com>
2023-03-28 21:34:50 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,8 +15,8 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
+      avatar: 'https://picx.zhimg.com/v2-7909661808b1ffd53c447b05ae277eae_r.jpg',
+      name: 'CUST-GPT',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
